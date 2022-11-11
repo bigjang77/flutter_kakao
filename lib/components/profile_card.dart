eec8466ab05d1ca7beed38e85bf361de05366a6f
bigjang77 <dhex2004@gmail.com>
2022-11-11 17:14:36 +0900

    카카오톡 완성

---
+++ lib/components/profile_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao/screens/profile_screen.dart';
+
+import '../models/user.dart';
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({Key? key, required this.user}) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                ProfileScreen(user: user), //프로필스크린에서 보여줄 user정보를 인수로 넘겨줍니다
+          ),
+        );
+      },
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 20,
+          backgroundImage: NetworkImage(
+            user.backgroundImage,
+          ),
+        ),
+        title: Text(
+          user.name,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        subtitle: Text(
+          user.intro,
+          style: TextStyle(fontSize: 12),
+        ),
+      ),
+    );
+  }
+}
